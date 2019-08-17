676d44f73eebf1a051ed5c5c16121e82722021e0
Dip Parmar <parmar.dpp@gmail.com>
2019-08-17 09:24:35 +0530

    Added page title and changed selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: 'ci-root',
+  template: `
+    <div>
+      <h1> {{pageTitle}} </h1>
+    </div>
+  `
 })
+
 export class AppComponent {
-  title = 'ci-front';
+  pageTitle: string = 'Chess Infnity';
 }
