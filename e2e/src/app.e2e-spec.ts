23bff193cd387d1ccc714d59db8459e6713dd270
Dip Parmar <parmar.dpp@gmail.com>
2019-08-18 15:04:44 +0530

    Fixed e2e test case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display site logo', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ci-front!');
+    expect(page.getLogo()).toEqual('CI');
   });
 
   afterEach(async () => {
